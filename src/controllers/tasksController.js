f52d0f819b0f5f9e972ed438f5760988b90b229a
joaoneto-hub <joaorodrigues8182@gmail.com>
2022-09-29 18:55:12 -0300

    first commit

---
+++ src/controllers/tasksController.js
@@ -0,0 +1,12 @@
+const taskModel = require('../models/taskModel');
+
+const getAll = async (request, response) => {
+
+  const tasks = await taskModel.getAll();
+
+  return response.status(200).json(tasks);
+};
+
+module.exports = {
+  getAll
+};
